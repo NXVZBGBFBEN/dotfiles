93c9d996eba60d8293bd5401ee68d51ba6a0aca5
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-08-10 15:00:17 +0900

    add $env:WAKATIME_HOME

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -1,5 +1,6 @@
 $env:PATH += ";$HOME\.local\bin"
 $env:EDITOR = "vim"
 $env:STARSHIP_CONFIG = "$HOME\.local\etc\starship\config.toml"
+$env:WAKATIME_HOME = "$HOME\.local\opt\wakatime"
 
 Invoke-Expression (&starship init powershell)
