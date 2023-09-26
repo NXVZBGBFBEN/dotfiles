ee71053a52e7fc909726a893fe7f0f50b64f457c
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-09-27 07:44:11 +0900

    define XDG directories

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -1,6 +1,24 @@
+$env:XDG_DATA_HOME = "$HOME\.local\share"
+$env:XDG_CONFIG_HOME = "$HOME\.local\etc"
+$env:XDG_STATE_HOME = "$HOME\.local\var\lib"
+$env:XDG_CACHE_HOME = "$HOME\.local\var\cache"
+
+$env:BIN_HOME = "$HOME\.local\bin"
+$env:OPT_HOME = "$HOME\.local\opt"
+
+
+##############################
+
+
 $env:PATH += ";$HOME\.local\bin"
 $env:EDITOR = "vim"
-$env:STARSHIP_CONFIG = "$HOME\.local\etc\starship\config.toml"
-$env:WAKATIME_HOME = "$HOME\.local\opt\wakatime"
+
+$env:STARSHIP_CONFIG = "$env:XDG_CONFIG_HOME\starship\config.toml"
+
+$env:WAKATIME_HOME = "$env:OPT_HOME\wakatime"
+
+
+##############################
+
 
 Invoke-Expression (&starship init powershell)
